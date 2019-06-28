c54dd1972cb159262a6911975686063a6bddb932
Kelvin <gobokel@gmail.com>
2019-06-28 13:31:26 +0100

    add gulp task to copy pdfjs files to build

--- preact.config.js
+++ preact.config.js
@@ -1,6 +1,10 @@
+// import CopyWebpackPlugin from 'copy-webpack-plugin';
+
 export default (config, env, helpers) => {
-  
 	if (env.isProd) {
+		// causes build error: cannot read property emit of undefined
+		// config.plugins.push( new CopyWebpackPlugin([{ context: `${__dirname}/src`, from: `*.*` }]) );
+
 		// Make async work
 		let babel = config.module.loaders.filter( loader => loader.loader === 'babel-loader')[0].options;
 		// Blacklist regenerator within env preset:
